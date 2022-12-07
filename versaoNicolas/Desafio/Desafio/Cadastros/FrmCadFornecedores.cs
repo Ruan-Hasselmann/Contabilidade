888960293310871c7f0bc45b6cefffdbcbcb061c
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2022-12-06 21:14:32 -0300

    Criação fornecedor

---
+++ versaoNicolas/Desafio/Desafio/Cadastros/FrmCadFornecedores.cs
@@ -0,0 +1,118 @@
+﻿using BaseDados;
+using Entidades;
+using System;
+using System.Windows.Forms;
+
+namespace Desafio.Cadastros
+{
+    public partial class FrmCadFornecedores : Form
+    {
+
+        private bool IsNovo;
+        private Fornecedor Cadastro;
+        public FrmCadFornecedores()
+        {
+            this.IsNovo = true;
+            InitializeComponent();
+        }
+
+        private void Bind()
+        {
+            this.TxtCodigo.DataBindings.Clear();
+            this.TxtDescricao.DataBindings.Clear();
+
+            this.TxtCodigo.DataBindings.Add("Text", this.Cadastro, "Codigo", false, DataSourceUpdateMode.OnPropertyChanged);
+            this.TxtDescricao.DataBindings.Add("Text", this.Cadastro, "Descricao", false, DataSourceUpdateMode.OnPropertyChanged);
+
+        }
+
+        private void FrmCadFornecedores_Load(object sender, EventArgs e)
+        {
+            if (IsNovo)
+                this.Cadastro = new Fornecedor();
+
+            Bind();
+        }
+
+        public FrmCadFornecedores(Fornecedor fornecedor)
+        {
+            InitializeComponent();
+            this.Cadastro = fornecedor;
+            IsNovo = false;
+        }
+
+
+
+        private void LimparControles()
+        {
+            TxtDescricao.Text = string.Empty;
+        }
+
+        private void Inserir()
+        {
+            try
+            {
+                Conexao _bd = new Conexao();
+                _bd.InserirFornecedor(this.Cadastro);
+
+                MessageBox.Show("Fornecedor Inserido Com Sucesso", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LimparControles();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Alterar()
+        {
+            try
+            {
+                Conexao _bd = new Conexao();
+               _bd.AlterarFornecedor(this.Cadastro);
+
+                MessageBox.Show("Fornecedor Alterado Com Sucesso", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Salvar()
+        {
+            if (!string.IsNullOrEmpty(TxtDescricao.Text))
+            {
+                if (IsNovo)
+                    Inserir();
+                else
+                    Alterar();
+
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                MessageBox.Show("Todos os campos devem ser preenchidos", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        //Buttons
+        private void BtnConfirmar_Click(object sender, EventArgs e)
+        {
+
+                Salvar();
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
